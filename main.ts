bfc7e21b1e77248dd6ec5860dde60a34869cc1d9
Juan David <120610426+JUAN57755@users.noreply.github.com>
2023-02-20 16:11:34 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,6 +19,9 @@ scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles8, function (s
     info.changeScoreBy(1)
     tiles.setTileAt(location, sprites.castle.tileGrass1)
 })
+controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.play(music.createSoundEffect(WaveShape.Square, 4020, 1044, 255, 255, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.swamp.swampTile9, function (sprite, location) {
     tiles.placeOnRandomTile(mySprite, assets.tile`myTile`)
     tiles.setCurrentTilemap(tilemap`level`)
